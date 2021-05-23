dffc4c7a2690e26987c7db247779155938499ffd
Tony Messias <tonysm@hey.com>
2021-05-23 18:25:21 -0300

    Entangle issue

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 class Counter extends Component
 {
     public int $value = 0;
+    public bool $tick = false;
 
     public function increment()
     {
@@ -16,10 +17,27 @@ class Counter extends Component
     public function render()
     {
         return <<<'blade'
-        <div>
-            <p>Counter: {{ $value }}</p>
+        <div
+            x-data="{
+                tick: @entangle('tick'),
+                timer: null,
+            }"
+            x-init="$watch('tick', function (value) {
+                if (this.timer) {
+                    clearInterval(this.timer);
+                    this.timer = null;
+                }
 
-            <button wire:click="increment">Inc</button>
+                if (value) {
+                    this.timer = setInterval(() => {
+                        $wire.increment();
+                    }, 2000);
+                }
+            })"
+        >
+            <p>Counter <span x-text="tick ? 'on' : 'off'"></span>: {{ $value }}</p>
+
+            <button @click="tick = !tick">Toggle Timer</button>
         </div>
         blade;
     }
